1aede603d7f0d426ae317d6114fac2306e2a9f46
opilane <opilane@HP-CZC2349HTF>
2023-09-12 15:37:31 +0300

    Add project files.

---
+++ Browser/Browser.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Browser
+{
+    private Stack<string> _back = new Stack<string>();
+    private Stack<string> _forward = new Stack<string>();
+    private List<string> _history = new List<string>();
+    private List<string> _bookmarks = new List<string>();
+
+    private string _current;
+
+    public Browser()
+    {
+        GoTo("google.com");
+    }
+
+    public void GoTo(string url)
+    {
+        _back.Push(_current);
+        _current = url;
+        _forward.Clear();
+        _history.Add(url);
+        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        Console.WriteLine("Laadimine: " + url + "\n");
+        Console.ResetColor();
+    }
+
+    public void Back()
+    {
+        if (_back.Count > 0)
+        {
+            _forward.Push(_current);
+            _current = _back.Pop();
+            _history.Add(_current);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\n Tagasi on: " + _current + "\n");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n Eelmist lehte pole. \n");
+            Console.ResetColor();
+        }
+    }
+
+    public void Forward()
+    {
+        if (_forward.Count > 0)
+        {
+            _back.Push(_current);
+            _current = _forward.Pop();
+            _history.Add(_current);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\n Edasi on: " + _current);
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n Ei mingit edasist lehte. \n");
+            Console.ResetColor();
+        }
+    }
+
+    public string Current()
+    {
+        return _current;
+    }
+
+    public List<string> History()
+    {
+        return _history;
+    }
+
+    public void RemoveHistory()
+    {
+        _history.Clear();
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Kustutatud külstusajaloo. \n");
+        Console.ResetColor();
+    }
+
+    public void AddBookmark()
+    {
+        _bookmarks.Add(_current);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Lisatud praegune leht järjehoidjatesse: " + _current);
+        Console.ResetColor();
+    }
+
+    public void ViewBookmarks()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Järjehoidjaid: " + string.Join(", ", _bookmarks));
+        Console.ResetColor();
+    }
+}
---
+++ Browser/Program.cs
@@ -0,0 +1,79 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Browser browser = new Browser();
+
+        while (true)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("Valige valik:");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("1. Minge uuele lehele");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("2. Tagasi");
+            Console.WriteLine("3. Edasi");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("4. Näita praegune lehekülg");
+            Console.WriteLine("5. Näita külstusajaloo");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("6. Add bookmark");
+            Console.WriteLine("7. Näita järjehoidjaid");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("8. Kustuta järjehoidjaid ja külstusajaloo");
+            Console.WriteLine("9. Lõpeta");
+            Console.ResetColor();
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write("Sisestage URL: ");
+                    Console.ResetColor();
+                    string url = Console.ReadLine();
+                    browser.GoTo(url);
+                    break;
+                case "2":
+                    browser.Back();
+                    break;
+                case "3":
+                    browser.Forward();
+                    break;
+                case "4":
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("Käesolev lehekülg: " + browser.Current());
+                    Console.ResetColor();
+                    break;
+                case "5":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Külastusajaloo: " + string.Join(", ", browser.History()));
+                    Console.ResetColor();
+                    break;
+                case "6":
+                    browser.AddBookmark();
+                    break;
+                case "7":
+                    browser.ViewBookmarks();
+                    break;
+                case "8":
+                    browser.RemoveHistory();
+                    break;
+                case "9":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Vale valik. Palun proovi uuesti.");
+                    Console.ResetColor();
+                    break;
+            }
+
+            // Пауза перед очисткой консоли
+            Console.WriteLine("Jätkamiseks vajutage sisestusklahvi...");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
